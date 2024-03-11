ad9e8dace6cd6dd97d3da89bc9a97d2502995124
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2024-03-11 01:47:47 +0000

    Added Foreing Key to Product Category

--- store/models.py
+++ store/models.py
@@ -20,6 +20,10 @@ class Category(models.Model):
 
 class Product(models.Model):
 
+    # FK
+
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE, null=True)
+
     title = models.CharField(max_length=250)
 
     brand = models.CharField(max_length=250, default='un-branded')
