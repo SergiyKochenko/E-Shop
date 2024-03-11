c753f4dd01691bd2d3bee9597b1cbdf5aef5ee17
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2024-03-11 22:27:54 +0000

    Added Category view and update base.html

--- store/views.py
+++ store/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+from . models import Category
+
 # Create your views here.
 
 def store(request):
 
     return render(request, 'store/store.html')
+
+def categories(request):
+
+    all_categories = Category.objects.all()
+
+    return {'all_categories': all_categories}
