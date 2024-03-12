0a7ce717f0f91b392b412742829b5fc9b1e12f27
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2024-03-12 00:11:29 +0000

    Creating ptoduct card info page

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
 
     path('', views.store, name='store'),
+
+    path('product/<slug:slug>/', views.product_info, name='product-info'),
+
 ]
--- store/views.py
+++ store/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from . models import Category, Product
 
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 
 def store(request):
@@ -17,3 +19,11 @@ def categories(request):
     all_categories = Category.objects.all()
 
     return {'all_categories': all_categories}
+
+def product_info(request, slug):
+
+    product = get_object_or_404(Product, slug=slug)
+
+    context = {'product' : product}
+
+    return render(request, 'store/product-info.html', context)
