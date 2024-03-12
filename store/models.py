63778f053692f3b6fde5584c2c59f49d8dbd611e
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2024-03-12 23:17:23 +0000

    Created list category view

--- store/models.py
+++ store/models.py
@@ -16,6 +16,10 @@ class Category(models.Model):
     def __str__(self):
 
         return self.name
+
+    def get_absolute_url(self):
+
+        return reverse('list-category', args=[self.slug])
     
 
 class Product(models.Model):
--- store/urls.py
+++ store/urls.py
@@ -4,8 +4,16 @@ from . import views
 
 urlpatterns = [
 
+    # Store main page
+
     path('', views.store, name='store'),
 
-    path('product/<slug:slug>/', views.product_info, name='product-info'),
+    # Individual product
+
+    path('product/<slug:product_slug>/', views.product_info, name='product-info'),
+
+    # Individual category
+
+    path('search/<slug:category_slug>/', views.list_category, name='list-category'),
 
 ]
