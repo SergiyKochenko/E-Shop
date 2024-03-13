cb2e465d095f6f77f6214eac7786015cea1f80df
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2024-03-13 00:16:59 +0000

    Created urls and views for shopping cart, templates, cart-summary.html

--- ecommerceshop/urls.py
+++ ecommerceshop/urls.py
@@ -22,9 +22,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+
+    # Admin app
+
     path('admin/', admin.site.urls),
 
+    # Store app
+
     path('', include('store.urls')),
+
+    # Cart app
+
+    path('cart/', include('cart.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
