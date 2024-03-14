7f6e73e0279b83ca2e392f4593bfedfa7f0e5a29
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2024-03-14 21:37:50 +0000

    Added delete functionality

--- cart/views.py
+++ cart/views.py
@@ -38,6 +38,28 @@ def cart_add(request):
 
 def cart_delete(request):
 
+    cart = Cart(request)
+
+    if request.POST.get('action') == 'post':
+
+        product_id = int(request.POST.get('product_id'))
+
+        cart.delete(product=product_id)
+
+        cart_quantity = cart.__len__()
+
+        cart_total = cart.get_total()
+
+        response = JsonResponse({'qty':cart_quantity, 'total':cart_total})
+
+        return response
+
+
+
+
+
+
+
     pass
 
 
