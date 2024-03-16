45bde4fd13cc6bc02251b199bc03e35e09f56243
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2024-03-16 02:46:30 +0000

    fixing bug env.py

--- ecommerceshop/settings.py
+++ ecommerceshop/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 
-
+if os.path.exists("env.py"):
+    import env
 
 from pathlib import Path
 
@@ -150,11 +151,24 @@ MEDIA_ROOT = BASE_DIR / 'static/media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_USE_TLS = 'True'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = '587'
+# EMAIL_USE_TLS = 'True'
 
 
-EMAIL_HOST_USER = 'sergiykochenko@gmail.com' # - Enter your GMAIL address # The host email that sends password reset emails
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS') # - Enter your app password 
+# EMAIL_HOST_USER = 'sergiykochenko@gmail.com' # - Enter your GMAIL address # The host email that sends password reset emails
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS') # - Enter your app password 
+
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'sergiykochenko@gmail.com.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
