cd3fb9855d08117296aa58fc05d14871e0ffa369
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2024-03-16 01:01:05 +0000

    Added email verification view functionality

--- account/views.py
+++ account/views.py
@@ -3,6 +3,17 @@ from django.shortcuts import redirect, render
 
 from .forms import CreateUserForm
 
+from django.contrib.sites.shortcuts import get_current_site
+
+from . token import user_tokenizer_generate
+
+from django.template.loader import render_to_string
+
+from django.utils.encoding import force_bytes, force_str
+
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+
+
 def register(request):
 
     form = CreateUserForm()
@@ -11,16 +22,34 @@ def register(request):
 
         form = CreateUserForm(request.POST)
 
-        if form.is_valid(): 
+        if form.is_valid():
 
             user = form.save()
 
-            return redirect('store')
+            user.is_active = False
+
+            user.save()
+
+            current_site = get_current_site(request)
+
+            subject = 'Account verification email'
+
+            message = render_to_string('account/registration/email-verification.html', {
+            
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': user_tokenizer_generate.make_token(user),
+            
+            })
+
+            user.email_user(subject=subject, message=message)
+
+            return redirect('email-verification-sent')
 
 
 
     context = {'form':form}
-
 
     return render(request, 'account/registration/register.html', context=context)
 
