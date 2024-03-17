944ad2c092bf266640e281490703e3a476acebea
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2024-03-17 23:39:57 +0000

    Created password reaset urls

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+from django.contrib.auth import views as auth_views
+
+
+
 urlpatterns = [
 
     path('register', views.register, name='register'),
@@ -23,5 +27,15 @@ urlpatterns = [
     path('profile-management', views.profile_management, name='profile-management'),
 
     path('delete-account', views.delete_account, name='delete-account'),
+
+
+
+    path('reset_password', auth_views.PasswordResetView.as_view(), name='reset_password'),
+
+    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 ]
