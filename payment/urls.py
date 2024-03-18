f87c291e625db7da719f3e8e7ac4d0a4bb48d619
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2024-03-18 23:44:51 +0000

    Created check out template

--- payment/urls.py
+++ payment/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
 
+    path('checkout', views.checkout, name='checkout'),
+
     path('payment-success', views.payment_success, name='payment-success'),
 
     path('payment-failed', views.payment_failed, name='payment-failed'),
--- payment/views.py
+++ payment/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def checkout(request):
+        
+        return render(request, 'payment/checkout.html')
 
 
 def payment_success(request):
 
-
     # Clear shopping cart
-
 
     for key in list(request.session.keys()):
 
         if key == 'session_key':
 
             del request.session[key]
-
-
 
     return render(request, 'payment/payment-success.html')
 
