607a21f45b62108aeeb12ae81fc80cce6c14360f
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2024-03-18 00:07:33 +0000

    Added the password reset templates

--- account/urls.py
+++ account/urls.py
@@ -30,12 +30,12 @@ urlpatterns = [
 
 
 
-    path('reset_password', auth_views.PasswordResetView.as_view(), name='reset_password'),
+     path('reset_password', auth_views.PasswordResetView.as_view(template_name="account/password/password-reset.html"), name='reset_password'),
 
-    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(template_name="account/password/password-reset-sent.html"), name='password_reset_done'),
 
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="account/password/password-reset-form.html"), name='password_reset_confirm'),
 
-    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(template_name="account/password/password-reset-complete.html"), name='password_reset_complete'),
 
 ]
