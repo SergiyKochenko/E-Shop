cba13ec680e03914f18f734bcbe12fde73adf931
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2024-03-19 00:34:48 +0000

    Added order and order item models

--- payment/admin.py
+++ payment/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from . models import ShippingAddress
+from . models import ShippingAddress, Order, OrderItem
 
 admin.site.register(ShippingAddress)
 
+admin.site.register(Order)
+
+admin.site.register(OrderItem)
