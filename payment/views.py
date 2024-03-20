7888bc055ffdaff52dd81ab529ec6515cde5bab8
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2024-03-20 01:49:10 +0000

    Commented out - email invoce for buyers, added coments

--- payment/views.py
+++ payment/views.py
@@ -6,17 +6,21 @@ from cart.cart import Cart
 
 from django.http import JsonResponse
 
-from django.core.mail import send_mail
+# from django.core.mail import send_mail
 
-from django.conf import settings
+# from django.conf import settings
 
 
 
 def checkout(request):
 
+    # Users with accounts -- Pre-fill the form
+
     if request.user.is_authenticated:
 
         try:
+
+            # Authenticated users WITH shipping information 
 
             shipping_address = ShippingAddress.objects.get(user=request.user.id)
 
@@ -26,9 +30,13 @@ def checkout(request):
 
         except:
 
+            # Authenticated users with NO shipping information
+
             return render(request, 'payment/checkout.html')
 
     else:
+            
+        # Guest users
 
         return render(request, 'payment/checkout.html')
 
@@ -48,13 +56,17 @@ def complete_order(request):
         state = request.POST.get('state')
         zipcode = request.POST.get('zipcode')
 
+        # All-in-one shipping address
+
         shipping_address = (address1 + "\n" + address2 + "\n" +
         
-        city + "\n" + state + "\n" + zipcode
-        
-        )
+        city + "\n" + state + "\n" + zipcode)
+
+        # Shopping cart information 
 
         cart = Cart(request)
+
+        # Get the total price of items
 
         total_cost = cart.get_total()
 
@@ -67,7 +79,10 @@ def complete_order(request):
 
             2) Create order -> Guest users without an account
         
+
         '''
+
+        # 1) Create order -> Account users WITH + WITHOUT shipping information
 
         if request.user.is_authenticated:
 
@@ -84,15 +99,16 @@ def complete_order(request):
                 price=item['price'], user=request.user)
 
 
-            # Email order
+            # # Email order
                 
             # send_mail('Order received', 'Hi! ' + '\n\n' + 'Thank you for placing your order' + '\n\n' +
                       
-            #           'Please see your order below:' + '\n\n' + str(all_products) + '\n\n' + 'Total paid: $' +
+            #     'Please see your order below:' + '\n\n' + str(all_products) + '\n\n' + 'Total paid: $' +
 
-            #           str(cart.get_total()), settings.EMAIL_HOST_USER, [email], fail_silently=False,)
-            
+            #     str(cart.get_total()), settings.EMAIL_HOST_USER, [email], fail_silently=False,)
 
+
+        #  2) Create order -> Guest users without an account
 
         else:
 
@@ -102,7 +118,7 @@ def complete_order(request):
 
             order_id = order.pk
 
-            product_list = []
+            # product_list = []
 
             for item in cart:
 
@@ -116,14 +132,13 @@ def complete_order(request):
 
             # all_products = product_list
 
-            # Email order
+            # # Email order
                 
             # send_mail('Order received', 'Hi! ' + '\n\n' + 'Thank you for placing your order' + '\n\n' +
                       
-            #           'Please see your order below:' + '\n\n' + str(all_products) + '\n\n' + 'Total paid: $' +
+            #     'Please see your order below:' + '\n\n' + str(all_products) + '\n\n' + 'Total paid: $' +
 
-            #           str(cart.get_total()), settings.EMAIL_HOST_USER, [email], fail_silently=False,)
-
+            #     str(cart.get_total()), settings.EMAIL_HOST_USER, [email], fail_silently=False,)
 
 
         order_success = True
@@ -145,7 +160,6 @@ def payment_success(request):
             del request.session[key]
 
     return render(request, 'payment/payment-success.html')
-
 
 
 def payment_failed(request):
